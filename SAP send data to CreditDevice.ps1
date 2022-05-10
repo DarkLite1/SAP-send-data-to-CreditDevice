cc49e2f779fdf06bbc46daba97a09ea14806ebd8
DarkLite1 <BrechtGijbels@gmail.com>
2022-05-10 14:43:19 +0200

    import with UTF8 encoding

--- SAP send data to CreditDevice.ps1
+++ SAP send data to CreditDevice.ps1
@@ -57,7 +57,7 @@ Begin {
             $totalLength = $String.length
             
             if ($Start -gt $totalLength) {
-                Exit
+                Return ''
             }
             
             $calculatedLength = $totalLength - $Start
@@ -175,12 +175,12 @@ Process {
         #region Get debtor and invoice file content
         $fileContent = @{
             debtor  = @{
-                raw       = Get-Content -LiteralPath $file.DebtorFile
-                converted = @()
+                raw       = Get-Content -LiteralPath $file.DebtorFile -Encoding UTF8
+                converted = [System.Collections.ArrayList]@()
             }
             invoice = @{
-                raw       = Get-Content -LiteralPath $file.InvoiceFile
-                converted = @()
+                raw       = Get-Content -LiteralPath $file.InvoiceFile -Encoding UTF8
+                converted = [System.Collections.ArrayList]@()
             }
         }
     
@@ -194,14 +194,16 @@ Process {
         $M = "Convert file debtor '$($file.DebtorFile)' to objects"
         Write-Verbose $M; Write-EventLog @EventVerboseParams -Message $M
             
-        $fileContent.debtor.converted += foreach (
+        foreach (
             $line in 
             $fileContent.debtor.raw
         ) {
             try {
+                Write-Verbose "line: $line"
                 $params = @{
                     String = $line
                 }
+                Get-StringHC @params -Start 682 -Length 3
                 If ($companyCode = Get-StringHC @params -Start 10 -Length 4) {
                     $creditExposure = if (
                         ($line.length -gt 654) -and
@@ -215,74 +217,38 @@ Process {
                     }
                     else { '' }
                     
-                    [PSCustomObject]@{
-                        DebtorNumber          = Get-StringHC @params -Start 0 -Length 10
-                        CompanyCode           = $companyCode
-                        Name                  = Get-StringHC @params -Start 14 -Length 35
-                        NameExtra             = Get-StringHC @params -Start 129 -Length 35
-                        Street                = Get-StringHC @params -Start 49 -Length 35
-                        PostalCode            = Get-StringHC @params -Start 84 -Length 10
-                        City                  = Get-StringHC @params -Start 94 -Length 35
-                        CountryCode           = Get-StringHC @params -Start 230 -Length 3
-                        CountryName           = Get-StringHC @params -Start 574 -Length 22
-                        PoBox                 = Get-StringHC @params -Start 164 -Length 18
-                        PoBoxPostalCode       = Get-StringHC @params -Start 182 -Length 10
-                        PoBoxCity             = Get-StringHC @params -Start 192 -Length 35
-                        PhoneNumber           = Get-StringHC @params -Start 233 -Length 16
-                        MobilePhoneNumber     = Get-StringHC @params -Start 249 -Length 16
-                        EmailAddress          = Get-StringHC @params -Start 265 -Length 50
-                        Comment               = Get-StringHC @params -Start 533 -Length 36
-                        CreditLimit           = Get-StringHC @params -Start 356 -Length 18
-                        VatRegistrationNumber = Get-StringHC @params -Start 377 -Length 20
-                        AccountGroup          = Get-StringHC @params -Start 442 -Length 4
-                        CustomerLanguage      = Get-StringHC @params -Start 446 -Length 1
-                        PaymentTerms          = Get-StringHC @params -Start 451 -Length 4
-                        DunsNumber            = Get-StringHC @params -Start 596 -Length 11
-                        Rating                = Get-StringHC @params -Start 682 -Length 3
-                        DbCreditLimit         = Get-StringHC @params -Start 621 -Length 20
-                        NextInReview          = Get-StringHC @params -Start 641 -Length 13
-                        RiskCategory          = Get-StringHC @params -Start 669 -Length 3
-                        CreditAccount         = Get-StringHC @params -Start 674 -Length 8
-                        CreditExposure        = $creditExposure
-                        # DebtorNumber          = $line.SubString(0, 10).Trim()
-                        # Name                  = $line.SubString(14, 35).Trim()
-                        # NameExtra             = $line.SubString(129, 35).Trim()
-                        # Street                = $line.SubString(49, 35).Trim()
-                        # PostalCode            = $line.SubString(84, 10).Trim()
-                        # City                  = $line.SubString(94, 35).Trim()
-                        # CountryCode           = $line.SubString(230, 3).Trim()             
-                        # CountryName           = $line.SubString(574, 22).Trim()
-                        # PoBox                 = $line.SubString(164, 18).Trim()
-                        # PoBoxPostalCode       = $line.SubString(182, 10).Trim()
-                        # PoBoxCity             = $line.SubString(192, 35).Trim()
-                        # PhoneNumber           = $line.SubString(233, 16).Trim()
-                        # MobilePhoneNumber     = $line.SubString(249, 16).Trim()
-                        # EmailAddress          = $line.SubString(265, 50).Trim()
-                        # Comment               = $line.SubString(533, 36).Trim()
-                        # CreditLimit           = $line.SubString(356, 18).Trim()
-                        # VatRegistrationNumber = $line.SubString(377, 20).Trim()
-                        # AccountGroup          = $line.SubString(442, 4).Trim()
-                        # CustomerLanguage      = $line.SubString(446, 1).Trim()
-                        # PaymentTerms          = $line.SubString(451, 4).Trim()
-                        # DunsNumber            = $line.SubString(596, 11).Trim()
-                        # Rating                = if ($line.length -gt 682) {
-                        #     $line.SubString(682, $line.length - 682).Trim()
-                        # }
-                        # else { '' }
-                        # DbCreditLimit         = $line.SubString(621, 20).Trim()
-                        # NextInReview          = if ($line.length -gt 641) {
-                        #     $line.SubString(641, 13).Trim()
-                        # }
-                        # else { '' }
-                        # RiskCategory          = if ($line.Length -gt 669) {
-                        #     $line.SubString(669, 3).Trim()
-                        # }
-                        # else { '' }
-                        # CreditAccount         = if ($line.length -gt 674) {
-                        #     $line.SubString(674, 8).Trim()
-                        # }
-                        # else { '' }
-                    }
+                    $null = $fileContent.debtor.converted.Add(
+                        [PSCustomObject]@{
+                            DebtorNumber          = Get-StringHC @params -Start 0 -Length 10
+                            CompanyCode           = $companyCode
+                            Name                  = Get-StringHC @params -Start 14 -Length 35
+                            NameExtra             = Get-StringHC @params -Start 129 -Length 35
+                            Street                = Get-StringHC @params -Start 49 -Length 35
+                            PostalCode            = Get-StringHC @params -Start 84 -Length 10
+                            City                  = Get-StringHC @params -Start 94 -Length 35
+                            CountryCode           = Get-StringHC @params -Start 230 -Length 3
+                            CountryName           = Get-StringHC @params -Start 574 -Length 22
+                            PoBox                 = Get-StringHC @params -Start 164 -Length 18
+                            PoBoxPostalCode       = Get-StringHC @params -Start 182 -Length 10
+                            PoBoxCity             = Get-StringHC @params -Start 192 -Length 35
+                            PhoneNumber           = Get-StringHC @params -Start 233 -Length 16
+                            MobilePhoneNumber     = Get-StringHC @params -Start 249 -Length 16
+                            EmailAddress          = Get-StringHC @params -Start 265 -Length 50
+                            Comment               = Get-StringHC @params -Start 533 -Length 36
+                            CreditLimit           = Get-StringHC @params -Start 356 -Length 18
+                            VatRegistrationNumber = Get-StringHC @params -Start 377 -Length 20
+                            AccountGroup          = Get-StringHC @params -Start 442 -Length 4
+                            CustomerLanguage      = Get-StringHC @params -Start 446 -Length 1
+                            PaymentTerms          = Get-StringHC @params -Start 451 -Length 4
+                            DunsNumber            = Get-StringHC @params -Start 596 -Length 11
+                            Rating                = Get-StringHC @params -Start 682 -Length 3
+                            DbCreditLimit         = Get-StringHC @params -Start 621 -Length 20
+                            NextInReview          = Get-StringHC @params -Start 641 -Length 13
+                            RiskCategory          = Get-StringHC @params -Start 669 -Length 3
+                            CreditAccount         = Get-StringHC @params -Start 674 -Length 8
+                            CreditExposure        = $creditExposure
+                        }
+                    )
                 }
             }    
             catch {
@@ -296,7 +262,7 @@ Process {
         $excelParams.TableName = 'Debtor'
         $fileContent.debtor.converted | Export-Excel @excelParams
 
-        $M = "Exported '$(($fileContent.debtor.converted | Measure-Object).Count)' rows to Excel file '$($excelParams.Path)'"
+        $M = "Exported '$($fileContent.debtor.converted.Count)' rows to Excel file '$($excelParams.Path)'"
         Write-Verbose $M; Write-EventLog @EventOutParams -Message $M
         #endregion
 
@@ -304,7 +270,7 @@ Process {
         $M = "Convert invoice file '$($file.InvoiceFile)' to objects"
         Write-Verbose $M; Write-EventLog @EventVerboseParams -Message $M
             
-        $fileContent.invoice.converted += foreach (
+        foreach (
             $line in 
             $fileContent.invoice.raw
         ) {
@@ -324,20 +290,22 @@ Process {
                     Default { '' }
                 }
 
-                [PSCustomObject]@{
-                    SapDocumentNumber = $sapDocumentNumber
-                    DebtorNumber      = $line.SubString(0, 10).Trim()
-                    CompanyCode       = $line.SubString(10, 4).Trim()
-                    BusinessArea      = $line.SubString(136, 4).Trim()
-                    DocumentType      = $documentType
-                    Reference         = $reference
-                    InvoiceNumber     = $invoiceNumber
-                    Description       = $line.SubString(75, 50).Trim()
-                    DocumentDate      = $line.SubString(35, 8).Trim()
-                    NetDueDate        = $line.SubString(125, 8).Trim()
-                    Amount            = $line.SubString(44, 16).Trim()
-                    Currency          = $line.SubString(133, 3).Trim()
-                }
+                $null = $fileContent.invoice.converted.Add(
+                    [PSCustomObject]@{
+                        SapDocumentNumber = $sapDocumentNumber
+                        DebtorNumber      = $line.SubString(0, 10).Trim()
+                        CompanyCode       = $line.SubString(10, 4).Trim()
+                        BusinessArea      = $line.SubString(136, 4).Trim()
+                        DocumentType      = $documentType
+                        Reference         = $reference
+                        InvoiceNumber     = $invoiceNumber
+                        Description       = $line.SubString(75, 50).Trim()
+                        DocumentDate      = $line.SubString(35, 8).Trim()
+                        NetDueDate        = $line.SubString(125, 8).Trim()
+                        Amount            = $line.SubString(44, 16).Trim()
+                        Currency          = $line.SubString(133, 3).Trim()
+                    }
+                )
             }    
             catch {
                 Write-Warning "Failed converting invoice data '$line': $_"
@@ -350,7 +318,7 @@ Process {
         $excelParams.TableName = 'Invoice'
         $fileContent.invoice.converted | Export-Excel @excelParams
 
-        $M = "Exported '$(($fileContent.invoice.converted | Measure-Object).Count)' rows to Excel file '$($excelParams.Path)'"
+        $M = "Exported '$($fileContent.invoice.converted.Count)' rows to Excel file '$($excelParams.Path)'"
         Write-Verbose $M; Write-EventLog @EventOutParams -Message $M
         #endregion
 
